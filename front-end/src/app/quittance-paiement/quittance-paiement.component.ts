c37b4172b4a7d0c590ca247902e941b9b81c7dc5
khallouki nouheyla <nouheyla17@gmail.com>
2021-05-25 01:15:32 +0100

    pdf quittance

--- front-end/src/app/quittance-paiement/quittance-paiement.component.ts
+++ front-end/src/app/quittance-paiement/quittance-paiement.component.ts
@@ -83,51 +83,86 @@ export class QuittancePaiementComponent implements OnInit {
     this.router.navigateByUrl('/paiement');
   }
 
-  public downloadAsPDF() {
-    /*console.log("yes ");
-    const doc = new jsPDF();*/
-
-
-    var doc = new jsPDF();
-    doc.setFontSize(18);
-    doc.setFillColor(30,144,255);
-    doc.text('HNFS',95, 15);
-    doc.setFontSize(16);
-    doc.text('Loyer de quittance', 80, 25);
-    doc.setFontSize(11);
-    doc.text('Bailleur :',13,35);
-    doc.text(this.proprietaire.civilite,13,45);
-    doc.text(this.proprietaire.nom,23,45);
+    public downloadAsPDF() {
+   
+   console.log("yes ");
+     var doc = new jsPDF();
+       doc.setFontSize(18);
+       doc.text('HNFS', 95, 25);
+       doc.text('Quittance de loyer', 85, 35);
+       doc.setFontSize(11);
+       doc.text('Quittance émise le ',141,55);
+       doc.text(this.facture.date_paiement,175,55);
+       doc.text('Bailleur : ',20,62);
+       doc.text(this.proprietaire.civilite,20,70);
+       doc.text(this.proprietaire.nom,30,70);
+       doc.text(this.proprietaire.prenom,44,70);
+       doc.text(this.proprietaire.adresse,20,78);
+       doc.text('Locataire :',150,88);
+       doc.text(this.locataire.civilite,150,96);
+       doc.text(this.locataire.nom,160,96);
+       doc.text(this.locataire.prenom,182,96);
+       doc.text(this.locataire.adresse,150,104);
+       doc.text('Quittance de loyer', 85, 114);
+   
+       doc.line(20,118,200,118);
+       doc.setLineWidth(0.1);
+       doc.text('Description',45,123);
+       doc.text('Montant',135,123);
+       doc.line(20,128,200,128);
+       doc.setLineWidth(0.1);
+       doc.text('Adresse du bien ',30,135);
+       doc.text(this.bien.adresse,120,135);
+     
+       doc.line(20,138,200,138);
+       doc.setLineWidth(0.1);
+   
+       doc.text('Loyer mensuel ',30,145);
+       doc.text(this.bien.loyer_mensuel.toString(),120,145);
+       doc.text(('DH'),130,145);
+     
+       doc.line(20,148,200,148);
+       doc.setLineWidth(0.1);
+   
+       doc.text('Syndic ',30,155);
+       doc.text(this.bien.syndic.toString(),120,155);
+       doc.text(('DH'),130,155);
+     
+       doc.line(20,158,200,158);
+       doc.setLineWidth(0.1);
+   
+       doc.text('Pour le mois  ',30,165);
+       doc.text(this.facture.mois_paiement,120,165);
+     
+       doc.line(20,168,200,168);
+       doc.setLineWidth(0.1);
+       
+       doc.text('Montant total  ',30,175);
+       doc.text(this.facture.montant_total,120,175);
+       doc.text(('DH'),130,145);
+     
+       doc.line(20,178,200,178);
+       doc.setLineWidth(0.1);
+       
+       doc.line(100,118,100,178);
+       doc.setLineWidth(0.1);
+       doc.line(20,118,20,178);
+       doc.setLineWidth(0.1);
+       doc.line(200,118,200,178);
+       doc.setLineWidth(0.1);
     
+        doc.text('Cette quittance annule tous les reçus qui auraient pu être établis précédemment en cas de paiement partiel',20,188);
+        doc.text(' du montant du présent terme. Elle est à conserver pendant trois ans par le locataire (article 7-1 de la loi n° 89-462 du 6 juillet 1989).',20,198)
+        doc.text(' 89-462 du 6 juillet 1989).',20,208)
+  
+       doc.setFontSize(11);
+       doc.setTextColor(100);
+           doc.output('dataurlnewwindow')
    
-    doc.setFontSize(11);
-    doc.setTextColor(100);
-
-     // below line for Open PDF document in new tab
-     doc.output('dataurlnewwindow')
-
-     // below line for Download PDF document
-    // doc.save('quittance.pdf');
-
-   
-   // const pdfTable = this.pdfTable.nativeElement;
-   
-    //var html = htmlToPdfmake(pdfTable.innerHTML);
-   /* var html = htmlToPdfmake(`
-    <div>
-      <h1>My title</h1>
-      <p>
-        This is a sentence with a <strong>bold word</strong>, <em>one in italic</em>,
-        and <u>one with underline</u>. And finally <a href="https://www.facebook.com">a link</a>.
-      </p>
-    </div>
-  `);*/
-    //var html = htmlToPdfmake(document.getElementById("myP").innerHTML);
-     
-    //const documentDefinition = { content: html };
-   // pdfMake.createPdf(documentDefinition).open(); 
-     
-  }
+        doc.save('quittance.pdf');
+        
+        
+     }
 
 
 }
